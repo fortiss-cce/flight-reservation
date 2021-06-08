a83e05d26d038688d351401c32b309064f7c0676
Daniel Elsner <daniel.elsner@tum.de>
2021-06-08 13:20:06 +0200

    Add factory method pattern

--- src/main/java/flight/reservation/plane/Helicopter.java
+++ src/main/java/flight/reservation/plane/Helicopter.java
@@ -1,21 +1,45 @@
 package flight.reservation.plane;
 
 public class Helicopter extends Aircraft {
-    private final int H1_PASSENGER_CAPACITY = 4;
-    private final int H2_PASSENGER_CAPACITY = 6;
-    private final int CREW_PASSENGER_CAPACITY = 2;
-    private final String H1_MODEL_IDENTIFIER = "H1";
-    private final String H2_MODEL_IDENTIFIER = "H2";
+    private static final int H1_PASSENGER_CAPACITY = 4;
+    private static final int H2_PASSENGER_CAPACITY = 6;
+    private static final int CREW_PASSENGER_CAPACITY = 2;
+    private static final String H1_MODEL_IDENTIFIER = "H1";
+    private static final String H2_MODEL_IDENTIFIER = "H2";
+
+    private Helicopter() {
+    }
 
     public Helicopter(String model) {
-        setModel(model);
-        setCrewCapacity(CREW_PASSENGER_CAPACITY);
+        // We don't want to break the existing API (public constructor) here
+        // What would be a reasonable way to implement the factory method pattern then?
+        // Does it make sense at all?
+        Helicopter helicopter;
         if (model.equals(H1_MODEL_IDENTIFIER)) {
-            setPassengerCapacity(H1_PASSENGER_CAPACITY);
+            helicopter = createH1Helicopter();
         } else if (model.equals(H2_MODEL_IDENTIFIER)) {
-            setPassengerCapacity(H2_PASSENGER_CAPACITY);
+            helicopter = createH2Helicopter();
         } else {
             throw new IllegalArgumentException(String.format(MODEL_TYPE_EXCEPTION_MESSAGE_FORMAT, model));
         }
+        setModel(helicopter.getModel());
+        setPassengerCapacity(helicopter.getPassengerCapacity());
+        setCrewCapacity(helicopter.getCrewCapacity());
+    }
+
+    public static Helicopter createH1Helicopter() {
+        Helicopter helicopter = new Helicopter();
+        helicopter.setModel(H1_MODEL_IDENTIFIER);
+        helicopter.setPassengerCapacity(H1_PASSENGER_CAPACITY);
+        helicopter.setCrewCapacity(CREW_PASSENGER_CAPACITY);
+        return helicopter;
+    }
+
+    public static Helicopter createH2Helicopter() {
+        Helicopter helicopter = new Helicopter();
+        helicopter.setModel(H2_MODEL_IDENTIFIER);
+        helicopter.setPassengerCapacity(H2_PASSENGER_CAPACITY);
+        helicopter.setCrewCapacity(CREW_PASSENGER_CAPACITY);
+        return helicopter;
     }
 }
