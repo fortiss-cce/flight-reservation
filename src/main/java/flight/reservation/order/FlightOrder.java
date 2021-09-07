a65dad2f5704f26739ef66df38bcf675e54167fd
Josue Page <josue.page@tum.de>
2021-09-07 15:34:51 +0200

    Created PaymentSTrategy base class, where credit card and paypal account derive, also modified flightOrder to pay regardless of the payment method.

--- src/main/java/flight/reservation/order/FlightOrder.java
+++ src/main/java/flight/reservation/order/FlightOrder.java
@@ -2,6 +2,7 @@ package flight.reservation.order;
 
 import flight.reservation.Customer;
 import flight.reservation.flight.ScheduledFlight;
+import flight.reservation.payment.PaymentStrategy;
 import flight.reservation.payment.CreditCard;
 import flight.reservation.payment.Paypal;
 
@@ -51,18 +52,14 @@ public class FlightOrder extends Order {
             return true;
         }
         // validate payment information
-        if (!cardIsPresentAndValid(creditCard)) {
+        if (creditCard == null) {
             throw new IllegalStateException("Payment information is not set or not valid.");
         }
-        boolean isPaid = payWithCreditCard(creditCard, this.getPrice());
+        boolean isPaid = this.pay(creditCard, this.getPrice());
         if (isPaid) {
             this.setClosed();
         }
         return isPaid;
-    }
-
-    private boolean cardIsPresentAndValid(CreditCard card) {
-        return card != null && card.isValid();
     }
 
     public boolean processOrderWithPayPal(String email, String password) throws IllegalStateException {
@@ -71,37 +68,22 @@ public class FlightOrder extends Order {
             return true;
         }
         // validate payment information
-        if (email == null || password == null || !email.equals(Paypal.DATA_BASE.get(password))) {
+        if (email == null || password == null) {
             throw new IllegalStateException("Payment information is not set or not valid.");
         }
-        boolean isPaid = payWithPayPal(email, password, this.getPrice());
+
+        // create new paypal account
+        PayPal paypalAccount = new PayPal(email, password);
+
+        
+        boolean isPaid = this.pay(paypalAccount, this.getPrice());
         if (isPaid) {
             this.setClosed();
         }
         return isPaid;
     }
 
-    public boolean payWithCreditCard(CreditCard card, double amount) throws IllegalStateException {
-        if (cardIsPresentAndValid(card)) {
-            System.out.println("Paying " + getPrice() + " using Credit Card.");
-            double remainingAmount = card.getAmount() - getPrice();
-            if (remainingAmount < 0) {
-                System.out.printf("Card limit reached - Balance: %f%n", remainingAmount);
-                throw new IllegalStateException("Card limit reached");
-            }
-            card.setAmount(remainingAmount);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean payWithPayPal(String email, String password, double amount) throws IllegalStateException {
-        if (email.equals(Paypal.DATA_BASE.get(password))) {
-            System.out.println("Paying " + getPrice() + " using PayPal.");
-            return true;
-        } else {
-            return false;
-        }
+    public boolean pay(PaymentStrategy paymentMethod, double amount) throws IllegalStateException {
+        return paymentMethod.pay(amount);
     }
 }
