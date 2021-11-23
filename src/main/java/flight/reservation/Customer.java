ea15d5bfdad8deff1e496cddbafe37d8339b721d
Christian Reinbold <->
2021-11-23 10:59:41 +0100

    Remove code duplication of valid order check.

--- src/main/java/flight/reservation/Customer.java
+++ src/main/java/flight/reservation/Customer.java
@@ -21,9 +21,6 @@ public class Customer {
     }
 
     public FlightOrder createOrder(List<String> passengerNames, List<ScheduledFlight> flights, double price) {
-        if (!isOrderValid(passengerNames, flights)) {
-            throw new IllegalStateException("Order is not valid");
-        }
         FlightOrder order = new FlightOrder(flights);
         order.setCustomer(this);
         order.setPrice(price);
@@ -33,23 +30,12 @@ public class Customer {
                 .collect(Collectors.toList());
         order.setPassengers(passengers);
         order.getScheduledFlights().forEach(scheduledFlight -> scheduledFlight.addPassengers(passengers));
+        if (!order.isValid()) {
+            throw new IllegalStateException("Order is not valid");
+        }
+
         orders.add(order);
         return order;
-    }
-
-    private boolean isOrderValid(List<String> passengerNames, List<ScheduledFlight> flights) {
-        boolean valid = true;
-        valid = valid && !FlightOrder.getNoFlyList().contains(this.getName());
-        valid = valid && passengerNames.stream().noneMatch(passenger -> FlightOrder.getNoFlyList().contains(passenger));
-        valid = valid && flights.stream().allMatch(scheduledFlight -> {
-            try {
-                return scheduledFlight.getAvailableCapacity() >= passengerNames.size();
-            } catch (NoSuchFieldException e) {
-                e.printStackTrace();
-                return false;
-            }
-        });
-        return valid;
     }
 
     public String getEmail() {
