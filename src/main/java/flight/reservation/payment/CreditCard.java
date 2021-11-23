dd808c970f002596ea61ca5887a7f54d37af5ec9
George Mesesan <georgpuerkel@googlemail.com>
2021-11-23 10:51:39 +0100

    refactored payment classes

--- src/main/java/flight/reservation/payment/CreditCard.java
+++ src/main/java/flight/reservation/payment/CreditCard.java
@@ -5,35 +5,51 @@ import java.util.Date;
 /**
  * Dummy credit card class.
  */
-public class CreditCard {
-    private double amount;
-    private String number;
-    private Date date;
-    private String cvv;
-    private boolean valid;
+public class CreditCard extends PaymentOption {
+	private double amount;
+	private String number;
+	private Date date;
+	private String cvv;
+	private boolean valid;
 
-    public CreditCard(String number, Date date, String cvv) {
-        this.amount = 100000;
-        this.number = number;
-        this.date = date;
-        this.cvv = cvv;
-        this.setValid();
-    }
+	public CreditCard(String number, Date date, String cvv) {
+		this.amount = 100000;
+		this.number = number;
+		this.date = date;
+		this.cvv = cvv;
+		this.setValid();
+	}
 
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
 
-    public double getAmount() {
-        return amount;
-    }
+	public double getAmount() {
+		return amount;
+	}
 
-    public boolean isValid() {
-        return valid;
-    }
+	public boolean isValid() {
+		return valid;
+	}
 
-    public void setValid() {
-        // Dummy validation
-        this.valid = number.length() > 0 && date.getTime() > System.currentTimeMillis() && !cvv.equals("000");
-    }
+	public void setValid() {
+		// Dummy validation
+		this.valid = number.length() > 0 && date.getTime() > System.currentTimeMillis() && !cvv.equals("000");
+	}
+
+	@Override
+	public boolean pay(double toPay) {
+		if (this.isValid()) {
+			System.out.println("Paying " + toPay + " using Credit Card.");
+			double remainingAmount = this.amount - toPay;
+			if (remainingAmount < 0) {
+				System.out.printf("Card limit reached - Balance: %f%n", remainingAmount);
+				throw new IllegalStateException("Card limit reached");
+			}
+			this.amount = remainingAmount;
+			return true;
+		} else {
+			throw new IllegalStateException("Payment information is not set or not valid.");
+		}
+	}
 }
